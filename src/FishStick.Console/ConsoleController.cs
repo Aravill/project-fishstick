fce5e61cd49f81abf29d3ba0a4ae4b919b0e3087
Petr Beneš <benesp37@cvut.cz>
2023-11-19 16:02:09 +0100

    #1: Added keyword highligthing. Hardcoded keywords.

--- src/FishStick.Console/ConsoleController.cs
+++ src/FishStick.Console/ConsoleController.cs
@@ -1,26 +1,46 @@
 using FishStick.Item;
 using FishStick.Scene;
+using System.Collections.Generic;
 
 namespace FishStick.Render
 {
   class ConsoleController
   {
+    private static readonly Dictionary<string, ConsoleColor> _keywords = new()
+    {
+      { "east", ConsoleColor.Red },
+      { "key", ConsoleColor.Blue }
+    };
+
 
     public static void WriteText(string text)
     {
-      WriteSlowly(text);
+      WriteSlowly(text)();
       Console.WriteLine();
     }
     public static void DescribeScene(IScene scene)
     {
-      WriteSlowly(scene.Description + " "); // Add a space to the end of the line
+      ConsoleWriter.Write(scene.Description)
+        .Slowly()
+        .WithHighlighting(_keywords)
+        .WithColor(ConsoleColor.DarkGray)
+        .ToConsole();
+
       foreach (ITransition transition in scene.Transitions)
       {
-        WriteSlowly(transition.Description + " ");
+        ConsoleWriter.Write(transition.Description)
+        .Slowly()
+        .WithHighlighting(_keywords)
+        .WithColor(ConsoleColor.Yellow)
+        .ToConsole();
       }
       foreach (IItem item in scene.Items)
       {
-        WriteSlowly(item.SceneDescription + " ");
+        ConsoleWriter.Write(item.SceneDescription)
+        .Slowly()
+        .WithHighlighting(_keywords)
+        .WithColor(ConsoleColor.DarkGray)
+        .ToConsole();
       }
       Console.WriteLine();
     }
@@ -39,9 +59,8 @@ namespace FishStick.Render
       return commandText;
     }
 
-    private static void WriteSlowly(string message)
+    private static Action WriteSlowly(string message) => () =>
     {
-      Console.ForegroundColor = ConsoleColor.DarkGray;
       foreach (char c in message)
       {
 
@@ -52,7 +71,74 @@ namespace FishStick.Render
         Console.Write(c);
         Thread.Sleep(20);
       }
-      Console.ForegroundColor = ConsoleColor.White;
+    };
+  }
+  public class ConsoleWriter
+  {
+    private string _message = string.Empty;
+    private ConsoleColor _foregroundColor = Console.ForegroundColor;
+    private ConsoleColor _backgroundColor = Console.BackgroundColor;
+    private bool _writeSlowly = false;
+    private Dictionary<string, ConsoleColor> _highlightedWords = new Dictionary<string, ConsoleColor>();
+
+    private ConsoleWriter() { }
+
+    public static ConsoleWriter Write(string message)
+    {
+      return new ConsoleWriter { _message = message };
+    }
+
+    public ConsoleWriter Slowly()
+    {
+      _writeSlowly = true;
+      return this;
+    }
+
+    public ConsoleWriter WithColor(ConsoleColor foregroundColor, ConsoleColor? backgroundColor = null)
+    {
+      _foregroundColor = foregroundColor;
+      _backgroundColor = backgroundColor ?? _backgroundColor;
+      return this;
+    }
+
+    public ConsoleWriter WithHighlighting(Dictionary<string, ConsoleColor> highlightedWords)
+    {
+      _highlightedWords = highlightedWords;
+      return this;
+    }
+
+    public void ToConsole()
+    {
+      Console.ForegroundColor = _foregroundColor;
+      Console.BackgroundColor = _backgroundColor;
+
+      string[] words = _message.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+      foreach (var word in words)
+      {
+        var keyword = word.Trim(new[] { ',', '.' });
+        bool isHighlighted = _highlightedWords.TryGetValue(keyword, out var highlightColor);
+        Console.ForegroundColor = isHighlighted ? highlightColor : _foregroundColor;
+
+        WriteWord(word, isHighlighted);
+
+        Console.Write(" ");
+      }
+
+      Console.ResetColor();
+    }
+
+    private void WriteWord(string word, bool isHighlighted)
+    {
+      foreach (char c in word)
+      {
+        Console.Write(c);
+        if (_writeSlowly)
+        {
+          Thread.Sleep(20);
+        }
+      }
     }
   }
+
 }
