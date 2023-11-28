0a2bfab5b7829a72bfa8a09246645a1d472ccb8b
Petr Beneš <87598878+Lorraxx@users.noreply.github.com>
2023-11-28 19:57:10 +0100

    Refactoring Console Controler and Asset loader (#12)
    * Refactoring ConsoleController
    
    * Refactoring AssetLoader
    
    * Separated classes to their own files

---
+++ src/FishStick.Console/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace FishStick.Render
+{
+  public static class StringExtensions
+{
+  public static string RemoveTagMarkers(this string text) => text.Replace("{", "").Replace("}", "");
+  public static string FindTaggedWords(this string text, out List<string> tags)
+  {
+    tags = Regex.Matches(text, @"\{([\w ]+)\}", RegexOptions.IgnoreCase).Select(match => match.Groups[1].Value).ToList();
+    return text;
+  }
+  public static IEnumerable<string> Concat(this string text, IEnumerable<string> next)
+  {
+    return toEnumerable(text).Concat(next);
+
+    // hax
+    IEnumerable<string> toEnumerable(string toEnum)
+    {
+      yield return toEnum;
+    }
+  }
+}
+}
