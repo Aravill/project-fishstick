17c78130dada165e8584a5612ca7cc0e90e45848
Aravill <mozikmichal@gmail.com>
2023-12-04 18:12:52 +0100

    basic scene movable panel

--- AvaloniaEditor/Views/MainWindow.axaml.cs
+++ AvaloniaEditor/Views/MainWindow.axaml.cs
@@ -1,30 +1,62 @@
 using System;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Shapes;
+using Avalonia.Input;
 using Avalonia.Interactivity;
 using Avalonia.Media;
+using MovableControl.Controls;
 
 namespace AvaloniaEditor.Views;
 
 public partial class MainWindow : Window
 {
+
+  private Panel? _selectedScenePanel;
   public MainWindow()
   {
     InitializeComponent();
+
   }
 
   public void AddSceneHandler(object sender, RoutedEventArgs args)
   {
     // Create a new box
-    var box = new Rectangle();
 
-    // Set the properties of the box
-    box.Width = 50;
-    box.Height = 50;
-    box.Fill = Brushes.Red;
-    box.Name = "Box";
 
     // Add the box to the canvas
-    SceneCanvas.Children.Add(box);
+    SceneCanvas.Children.Add(CreateScenePanel());
+  }
+
+  private ScenePanel CreateScenePanel()
+  {
+    var panel = new ScenePanel();
+    var stackPanel = new StackPanel();
+    // Set the properties of the box
+    panel.Width = 300;
+    panel.Height = 400;
+    panel.Background = Brushes.DimGray;
+
+    Border panelBorder = new Border();
+    panelBorder.BorderBrush = Brushes.Black;
+    panelBorder.BorderThickness = new Thickness(2);
+    panelBorder.Padding = new Thickness(2);
+
+    TextBlock descriptionLabel = new TextBlock();
+    descriptionLabel.Margin = new Thickness(0.5);
+    descriptionLabel.Text = "Scene Description:";
+    TextBox descriptionField = new TextBox();
+    descriptionField.Margin = new Thickness(0.5);
+    descriptionField.Watermark = "Description";
+    descriptionField.Height = 100;
+    descriptionField.AcceptsReturn = true;
+    descriptionField.TextWrapping = TextWrapping.Wrap;
+
+    panel.Children.Add(panelBorder);
+    stackPanel.Children.Add(descriptionLabel);
+    stackPanel.Children.Add(descriptionField);
+    panel.Children.Add(stackPanel);
+
+    return panel;
   }
 }
