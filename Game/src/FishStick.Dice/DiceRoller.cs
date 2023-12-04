737bd6912ee8320955891ea1521cfeee7a71932d
Petr Beneš <benespetr42@gmail.com>
2023-12-04 14:52:31 +0100

    Dice rolling refactors

--- Game/src/FishStick.Dice/DiceRoller.cs
+++ Game/src/FishStick.Dice/DiceRoller.cs
@@ -1,23 +1,11 @@
-namespace FishStick.Dice
+﻿namespace FishStick.Dice
 {
-  class DiceRoller
+  internal static class DiceRoller
   {
     public static int Roll(string diceNotation)
     {
-      string[] diceParts = diceNotation.Split('d');
-      int sides = int.Parse(diceParts[1]);
-      int diceCount = int.Parse(diceParts[0]);
-      int total = 0;
-      for (int i = 0; i < diceCount; i++)
-      {
-        total += Roll(sides);
-      }
-      return total;
-    }
-
-    public static int Roll(int sides)
-    {
-      return Random.Shared.Next(1, sides + 1);
+      DiceSet diceSet = diceNotation;
+      return diceSet.Roll();
     }
   }
 }
---
+++ Game/src/FishStick.Dice/DiceSet.cs
@@ -0,0 +1,32 @@
+﻿namespace FishStick.Dice
+{
+  record DiceSet(int Sides, int Count = 1)
+  {
+    public DiceSet(string diceNotation) : this(Parse(diceNotation)) { }
+
+    public static implicit operator DiceSet(string diceNotation) => Parse(diceNotation);
+
+    private static DiceSet Parse(string diceNotation)
+    {
+      string[] parts = diceNotation.Split('d');
+
+      return parts.Length switch
+      {
+        1 when int.TryParse(parts[0], out int sides)
+            => new DiceSet(sides),
+
+        2 when int.TryParse(parts[0], out int diceCount) && int.TryParse(parts[1], out int sides)
+            => new DiceSet(sides, diceCount),
+
+        _ => throw new ArgumentException("Invalid dice notation format.")
+      };
+    }
+
+    public int Roll() => 
+      Enumerable.Range(0, Count)
+                .Select(_ => Roll(Sides))
+                .Sum();
+
+    private static int Roll(int sides) => Random.Shared.Next(1, sides + 1);
+  }
+}
