96d4f4cae95739d1ef49811b46dff345d6a321da
Aravill <mozikmichal@gmail.com>
2023-12-04 17:06:54 +0100

    Add square on button click

--- AvaloniaEditor/Views/MainWindow.axaml.cs
+++ AvaloniaEditor/Views/MainWindow.axaml.cs
@@ -1,11 +1,30 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
+using Avalonia.Interactivity;
+using Avalonia.Media;
 
 namespace AvaloniaEditor.Views;
 
 public partial class MainWindow : Window
 {
-    public MainWindow()
-    {
-        InitializeComponent();
-    }
+  public MainWindow()
+  {
+    InitializeComponent();
+  }
+
+  public void AddSceneHandler(object sender, RoutedEventArgs args)
+  {
+    // Create a new box
+    var box = new Rectangle();
+
+    // Set the properties of the box
+    box.Width = 50;
+    box.Height = 50;
+    box.Fill = Brushes.Red;
+    box.Name = "Box";
+
+    // Add the box to the canvas
+    SceneCanvas.Children.Add(box);
+  }
 }
