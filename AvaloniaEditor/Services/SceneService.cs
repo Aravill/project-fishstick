7b3b15e9cf7aaf521da0b77f3090068f6b511e68
Aravill <mozikmichal@gmail.com>
2023-12-08 19:43:03 +0100

    persisting scene state

--- AvaloniaEditor/Services/SceneService.cs
+++ AvaloniaEditor/Services/SceneService.cs
@@ -1,15 +1,67 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Text.Json;
 using AvaloniaEditor.Models;
 
 namespace AvaloniaEditor.Services
 {
   public class SceneService
   {
-    public IEnumerable<Scene> GetItems() => new[]
+
+    private IEnumerable<Scene> _scenes;
+
+    public SceneService()
     {
+      _scenes = Array.Empty<Scene>();
+      LoadScenes();
+    }
+    public IEnumerable<Scene> GetItems()
+    {
+      return _scenes;
+    }
+    public void SaveScenes()
+    {
+      MemoryStream stream = new MemoryStream();
+      var ser = new DataContractJsonSerializer(typeof(IEnumerable<Scene>));
+      ser.WriteObject(stream, _scenes);
+      byte[] file = stream.ToArray();
+      stream.Close();
+      File.WriteAllBytes("Scenes.json", file);
+    }
+
+    private bool SceneFileExists()
+    {
+      return File.Exists("Scenes.json");
+    }
+
+    private void LoadScenes()
+    {
+      if (SceneFileExists())
+      {
+        IEnumerable<Scene>? deserializedList = Array.Empty<Scene>();
+        MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(File.ReadAllText("Scenes.json")));
+        var ser = new DataContractJsonSerializer(deserializedList.GetType());
+        deserializedList = ser.ReadObject(stream) as IEnumerable<Scene>;
+        stream.Close();
+        if (deserializedList != null)
+        {
+          _scenes = deserializedList;
+        }
+      }
+      else
+      {
+        _scenes = new[]
+            {
             new Scene { Id = "1", Description = "Room 1", Position = new Avalonia.Point(100, 100) },
             new Scene { Id = "2", Description = "Room 2", Position = new Avalonia.Point(200, 200) },
             new Scene { Id = "3", Description = "Room 3", Position = new Avalonia.Point(300, 300) },
         };
+      }
+    }
   }
+
+
 }
--- AvaloniaEditor/ViewModels/MainWindowViewModel.cs
+++ AvaloniaEditor/ViewModels/MainWindowViewModel.cs
@@ -3,28 +3,41 @@ using System;
 using System.Reactive.Linq;
 using AvaloniaEditor.Models;
 using AvaloniaEditor.Services;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
 
 namespace AvaloniaEditor.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
   private ViewModelBase _contentViewModel;
+  private SceneService _sceneService;
   public SceneViewModel ScenesView { get; }
 
 
   public MainWindowViewModel()
   {
-    var service = new SceneService();
-    var scenes = service.GetItems();
+    _sceneService = new SceneService();
+    var scenes = _sceneService.GetItems();
 
     ScenesView = new SceneViewModel(scenes);
     _contentViewModel = ScenesView;
+
+    if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktopLifetime)
+    {
+      desktopLifetime.Exit += OnExit;
+    }
   }
 
   public ViewModelBase ContentViewModel
   {
     get => _contentViewModel;
     private set => this.RaiseAndSetIfChanged(ref _contentViewModel, value);
+  }
+
+  private void OnExit(object? sender, ControlledApplicationLifetimeExitEventArgs e)
+  {
+    _sceneService.SaveScenes();
   }
 
   public void AddScene()
@@ -39,7 +52,7 @@ public class MainWindowViewModel : ViewModelBase
         {
           if (newScene != null)
           {
-            ScenesView.ListScenes.Add(newScene);
+            ScenesView.Scenes.Add(newScene);
           }
           ContentViewModel = ScenesView;
         });
