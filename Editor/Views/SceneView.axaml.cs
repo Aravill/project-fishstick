58b0d1768ca32e65dd53a6ad63280b9d19cc3778
Aravill <mozikmichal@gmail.com>
2023-12-10 10:55:25 +0100

    now highlighting selected cards

--- Editor/Views/SceneView.axaml.cs
+++ Editor/Views/SceneView.axaml.cs
@@ -1,13 +1,51 @@
 
+using System;
+using System.Drawing;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
+using AvaloniaEditor.Controls;
+using AvaloniaEditor.Helpers;
 
 namespace AvaloniaEditor.Views;
 
 public partial class SceneView : UserControl
 {
 
+  private ScenePanel? _selectedScenePanel { get; set; }
   public SceneView()
   {
     InitializeComponent();
+  }
+
+  public void Canvas_PointerPressed(object sender, PointerPressedEventArgs e)
+  {
+    // Wherever the user clicks, we deselect anyway
+    DeselectScenePanel();
+    // Check if the cursor was over a scene panel
+    Control? control = (Control?)e.Source;
+    if (control == null)
+      return;
+    ScenePanel? scenePanel = ControlUtils.FindParentOfType<ScenePanel>(control);
+    if (scenePanel != null)
+    {
+      SelectScenePanel(scenePanel);
+    }
+  }
+
+  public void SelectScenePanel(ScenePanel scenePanel)
+  {
+    DeselectScenePanel();
+    _selectedScenePanel = scenePanel;
+    _selectedScenePanel.Select();
+  }
+
+  public void DeselectScenePanel()
+  {
+    if (_selectedScenePanel == null)
+      return;
+    // Return selected panel back to normal background color
+    _selectedScenePanel.Deselect();
+    _selectedScenePanel = null;
   }
 }
